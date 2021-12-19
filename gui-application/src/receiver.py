1f8849877029e7286a3fdcbcb14838b6492c2d25
Marian Skut <marianskut@student.agh.edu.pl>
2021-12-19 20:15:03 +0100

    Fix in current calculation in the receiver

--- gui-application/src/receiver.py
+++ gui-application/src/receiver.py
@@ -1,16 +1,13 @@
-from typing import Union
-
 import serial as serial
 
 range_dict = {}
 type_dict = {}
 sign_dict = {}
-#XXX: Range needs fix
-range_dict["A"] = 10^-6
-range_dict["B"] = 10^-4
-range_dict["C"] = 10^-3
-range_dict["D"] = 10^-2
-range_dict["E"] = 1
+range_dict["A"] = 0.00001
+range_dict["B"] = 0.0001
+range_dict["C"] = 0.001
+range_dict["D"] = 0.01
+range_dict["E"] = 0.1
 range_dict["G"] = "Digit 0 is not BCD"
 range_dict["H"] = "Digit 1 is not BCD"
 range_dict["I"] = "Digit 2 is not BCD"
@@ -32,14 +29,18 @@ def decodePacket(self):
     measured_type = ""
     measured_sign = 1
     measurement_result = 0
-    
+
     try:
-        if type(range_dict[self.serial_buffer[1]]) is int:
-            measured_range: Union[int, str] = range_dict[self.serial_buffer[1]]
-        else:
-            # Probably some error
+        if type(range_dict[self.serial_buffer[1]]) is float:
+            measured_range = range_dict[self.serial_buffer[1]]
+            #Signal overload in application
+        elif self.serial_buffer[1] == "O":
+            self.measurement.set('Overload!')
             return
-            
+        else:
+            # Probably some transmission error
+            return          
+
         measured_type = type_dict[self.serial_buffer[2]]
         measured_sign = sign_dict[self.serial_buffer[3]]
         
@@ -50,28 +51,47 @@ def decodePacket(self):
         measurement_result += int(self.serial_buffer[8]) * 1
         measurement_result *= measured_sign
         measurement_result *= measured_range
-# tu dopisałem amperomierz
+        # Voltage mode - round to prevent floating-point noise and return result
         if self.measurement_mode.get() == "V":
-            self.measurement.set(str(measurement_result) + "V " + measured_type)
-        else:
-            if measured_range != 1:
-                self.measurement.set("Ustaw zakres 100mV")
+            self.raw_measurement = measurement_result
+            # Offset for REL function
+            measurement_result -= self.measurement_offset
+            
+            if abs(measurement_result) < 1.0:
+                self.measurement.set(str(round(measurement_result*1000, 3)) + " mV " + measured_type)
             else:
-                measured_current = (measurement_result/self.shunt_voltage) * self.shunt_range
-                if measured_current > 1.2 * self.shunt_range:
-                    self.measurement.set('Overload!')
+                self.measurement.set(str(round(measurement_result, 3)) + " V " + measured_type)
+        else:
+            try:
+                if measured_range != 0.00001:
+                    self.measurement.set("Ustaw zakres \n 100mV")
                 else:
-                    self.measurement.set(str(measured_current) + " A" + measured_type)
-        
-        print(self.measurement.get())
+                    # Calculate result according to Ohm's Law
+                    measured_current = (measurement_result/((int(self.shunt_voltage_list.get())) * 0.001)) \
+                        * int(self.shunt_range_list.get())
+                    self.raw_measurement = measured_current
+                    # Offset for REL function
+                    measured_current -= self.measurement_offset
+
+                    if measured_current > 1.2 * int(self.shunt_range_list.get()):
+                        self.measurement.set('Overload!')
+                    else:
+                        if abs(measured_current) < 1.0:
+                            self.measurement.set(str(round(measured_current*1000, 3)) + " mA " + measured_type)
+                        else:
+                            self.measurement.set(str(round(measured_current, 3)) + " A " + measured_type)
+            except:
+                return
+        # For debugging purposes
+        #print(self.measurement.get())
     except KeyError:
         print("Invalid frame")
-    
+
 def initSerialBuffer(self):
     self.serial_buffer = [None] * 11
     self.synced = False
     self.packet_pos = 0
-    
+
 def receiveTask(self):
     #Read buffer and update measured fields
     while True:
@@ -88,10 +108,10 @@ def receiveTask(self):
             break
         if (self.synced and c != '$'):
             continue;
-        
+
         # We have '$' on input - sync packet
         synced = True
-     
+
         # Add characters to buffer
         if (self.packet_pos > 0 or c == '$'):
             self.serial_buffer[self.packet_pos] = c
